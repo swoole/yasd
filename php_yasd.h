b916a50992c21a4ba5381d24647d3a1ad472c881
codinghuang <2812240764@qq.com>
2020-12-02 19:02:29 +0800

    Update license

--- php_yasd.h
+++ php_yasd.h
@@ -1,18 +1,18 @@
-/**
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+  +----------------------------------------------------------------------+
+  | Yasd                                                                 |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 2.0 of the Apache license,    |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.apache.org/licenses/LICENSE-2.0.html                      |
+  | If you did not receive a copy of the Apache2.0 license and are unable|
+  | to obtain it through the world-wide-web, please send a note to       |
+  | license@swoole.com so we can mail you a copy immediately.            |
+  +----------------------------------------------------------------------+
+  | Author: codinghuang  <codinghuang@qq.com>                            |
+  +----------------------------------------------------------------------+
+*/
 
 #ifndef PHP_YASD_H_
 #define PHP_YASD_H_
