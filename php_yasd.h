e15abb5e495dc9e015016d115b187b5148357398
codinghuang <2812240764@qq.com>
2020-12-23 10:52:05 +0800

    Update version for Yasd 0.2.7-alpha

--- php_yasd.h
+++ php_yasd.h
@@ -22,7 +22,7 @@
 extern zend_module_entry yasd_module_entry;
 #define phpext_yasd_ptr &yasd_module_entry
 
-#define PHP_YASD_VERSION "0.2.6"
+#define PHP_YASD_VERSION "0.2.7-alpha"
 
 ZEND_BEGIN_MODULE_GLOBALS(yasd)
     char *breakpoints_file;
